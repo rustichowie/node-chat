387ed05219c32b0c523fd35f2d524744cb659aa2
dunkmaster <haavard.olsen@live.com>
2014-12-12 11:30:27 +0100

    chat

---
+++ server.js
@@ -0,0 +1,43 @@
+var mongo = require('mongodb').MongoClient,
+	client = require('socket.io').listen(8081).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
+	if(err) throw err;
+	client.on('connection', function(socket) {
+		//wait for input
+		var col = db.collection('messages'),
+			sendStatus = function(s){
+				socket.emit('status', s);
+			};
+
+		//Emit messages
+		col.find().limit(100).sort({_id: 1}).toArray(function(err, res){
+			if(err) throw err;
+			socket.emit('init', res);
+		});
+
+		socket.on('input', function(data){
+			var name = data.name,
+				message = data.message,
+				whitespacePattern = /^\s*$/;
+			if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
+				sendStatus("Name and message is required.");
+			} else {
+				col.insert({name: name, message: message}, function(){
+					//Emit lates message to ALL clients
+					socket.broadcast.emit('output', [data]);
+					sendStatus({
+						message: "Message sent",
+						clear: true
+					});
+				});
+			}
+
+			
+		});
+	});
+
+});
+
+//var socket = io.connect('http://127.0.0.1:8081');
+
